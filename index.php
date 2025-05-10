1f44f06fe5b83716aeaacf0dc2cc653a94c10d82
Lyan Nazhabil Dzuquwwa <140893531+Ilyessin12@users.noreply.github.com>
2025-05-10 23:38:41 +0700

    changed index page to properly display CRUD operations

--- index.php
+++ index.php
@@ -7,4 +7,43 @@
 include("view/TampilMahasiswa.php");
 
 $tp = new TampilMahasiswa();
+$prosesmahasiswa = new ProsesMahasiswa();
+
+// CRUD Operations
+if(isset($_POST['add'])){
+	// Proses tambah data
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$tempat = $_POST['tempat'];
+	$tl = $_POST['tl'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$telp = $_POST['telp'];
+	
+	$prosesmahasiswa->add($nim, $nama, $tempat, $tl, $gender, $email, $telp);
+	header("location:index.php");
+}
+else if(isset($_POST['update'])){
+	// Proses update data
+	$id = $_POST['id'];
+	$nim = $_POST['nim'];
+	$nama = $_POST['nama'];
+	$tempat = $_POST['tempat'];
+	$tl = $_POST['tl'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
+	$telp = $_POST['telp'];
+	
+	$prosesmahasiswa->update($id, $nim, $nama, $tempat, $tl, $gender, $email, $telp);
+	header("location:index.php");
+}
+else if(isset($_POST['delete'])){
+	// Proses delete data
+	$id = $_POST['id'];
+	
+	$prosesmahasiswa->delete($id);
+	header("location:index.php");
+}
+
+// Tampilkan data
 $data = $tp->tampil();
