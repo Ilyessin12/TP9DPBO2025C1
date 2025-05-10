3dd2822dc04144b91acbed99a8af88d36000e42f
Lyan Nazhabil Dzuquwwa <140893531+Ilyessin12@users.noreply.github.com>
2025-05-10 23:38:27 +0700

    modified the view to display CRUD operations and to be able to perform it

--- view/TampilMahasiswa.php
+++ view/TampilMahasiswa.php
@@ -24,21 +24,57 @@ class TampilMahasiswa implements KontrakView
 		$data = null;
 
 		//semua terkait tampilan adalah tanggung jawab view
-		for ($i = 0; $i < $this->prosesmahasiswa->getSize(); $i++) {
+		for($i = 0; $i < $this->prosesmahasiswa->getSize(); $i++){
 			$no = $i + 1;
+			$id = $this->prosesmahasiswa->getId($i);
+			$nim = $this->prosesmahasiswa->getNim($i);
+			$nama = $this->prosesmahasiswa->getNama($i);
+			$tempat = $this->prosesmahasiswa->getTempat($i);
+			$tl = $this->prosesmahasiswa->getTl($i);
+			$gender = $this->prosesmahasiswa->getGender($i);
+			$email = $this->prosesmahasiswa->getEmail($i);
+			$telp = $this->prosesmahasiswa->getTelp($i);
+			
 			$data .= "<tr>
 			<td>" . $no . "</td>
-			<td>" . $this->prosesmahasiswa->getNim($i) . "</td>
-			<td>" . $this->prosesmahasiswa->getNama($i) . "</td>
-			<td>" . $this->prosesmahasiswa->getTempat($i) . "</td>
-			<td>" . $this->prosesmahasiswa->getTl($i) . "</td>
-			<td>" . $this->prosesmahasiswa->getGender($i) . "</td> </tr>";
+			<td>" . $nim . "</td>
+			<td>" . $nama . "</td>
+			<td>" . $tempat . "</td>
+			<td>" . $tl . "</td>
+			<td>" . $gender . "</td>
+			<td>" . $email . "</td>
+			<td>" . $telp . "</td>
+			<td>
+				<button class='btn btn-warning btn-sm' data-toggle='modal' data-target='#formModal' data-action='edit'
+					data-id='" . $id . "' 
+					data-nim='" . $nim . "' 
+					data-nama='" . $nama . "' 
+					data-tempat='" . $tempat . "' 
+					data-tl='" . $tl . "' 
+					data-gender='" . $gender . "' 
+					data-email='" . $email . "' 
+					data-telp='" . $telp . "'
+				>Edit</button>
+				<button class='btn btn-danger btn-sm' data-toggle='modal' data-target='#deleteModal' data-id='" . $id . "'>Hapus</button>
+			</td>
+			</tr>";
 		}
 		// Membaca template skin.html
 		$this->tpl = new Template("templates/skin.html");
 
 		// Mengganti kode Data_Tabel dengan data yang sudah diproses
 		$this->tpl->replace("DATA_TABEL", $data);
+		$this->tpl->replace("DATA_ID", "value=''");
+		$this->tpl->replace("DATA_NIM", "value=''");
+		$this->tpl->replace("DATA_NAMA", "value=''");
+		$this->tpl->replace("DATA_TEMPAT", "value=''");
+		$this->tpl->replace("DATA_TL", "value=''");
+		$this->tpl->replace("DATA_GENDER_L", "selected");
+		$this->tpl->replace("DATA_GENDER_P", "");
+		$this->tpl->replace("DATA_EMAIL", "value=''");
+		$this->tpl->replace("DATA_TELP", "value=''");
+		$this->tpl->replace("DATA_MODAL_TITLE", "Tambah Mahasiswa");
+		$this->tpl->replace("DATA_SUBMIT_TYPE", "add");
 
 		// Menampilkan ke layar
 		$this->tpl->write();
